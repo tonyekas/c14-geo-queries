9e594fefaab799f8f234a41639ef91f3e1f37926
Robert <r.starak@hotmail.com>
2025-04-11 15:01:27 -0600

    findTrafficCameraNear added

--- server/models/trafficCamera.js
+++ server/models/trafficCamera.js
@@ -61,16 +61,16 @@ export async function findCameraLocationByName(cameraNameAsset) {
 const METERS_PER_DEGREE = 10000000/90 
 
 export async function findCameraLocationNear(lat, lon, distanceM) {
-    const locations = await CameraLocation.find({
-      point: {
-        $near: {
-          $geometry: {
-            type: "Point",
-            coordinates: [lon, lat]
-          },
-          $maxDistance: distanceM
-        }
-      }
-    });
+    const distanceDegrees = distanceM / METERS_PER_DEGREE
+    const locations = await CameraLocation.find()
+    .where('point')
+    .within()
+    .circle({ center: [lon, lat], radius: distanceDegrees})
     return locations;
   }
+
+//   export async function findCameraLocationInBoundingBox(northLat, eastLon, southLat, westLon) {
+    
+
+//   }
+   
