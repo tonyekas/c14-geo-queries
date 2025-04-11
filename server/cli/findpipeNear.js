e13e761ab17183ea47b17c4fed79870761ac117c
Gia4a <giaanluu@gmail.com>
2025-04-11 14:41:02 -0600

    dsss

---
+++ server/cli/findpipeNear.js
@@ -0,0 +1,18 @@
+import { findPlaygroundEquipmentNear } from "../models/waterPipe.js"
+import { disconnectDb } from "../db.js"
+
+
+const lat = Number.parseFloat(process.argv[2]);
+const lon = Number.parseFloat(process.argv[3]);
+const distanceM = Number.parseFloat(process.argv[4]);
+
+const waterserviceaddress = await findpipeNear(lat, lon, distanceM);
+    for (const waterserviceaddress of pipes) {
+console.log(waterserviceaddress, '.', lat, lon, distanceM)
+}
+console.log('found', waterserviceaddress.address, waterserviceaddress.distance, 'waterserviceaddress')
+
+
+    await disconnectDb()
+
+
