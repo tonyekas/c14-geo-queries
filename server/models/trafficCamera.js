35d071faf24178f455428164acf5dfd04ec61653
tonyekas <tonyekas@yahoo.com>
2025-04-11 13:32:54 -0600

    traffic js update

--- server/models/trafficCamera.js
+++ server/models/trafficCamera.js
@@ -2,53 +2,75 @@ import { connectDb } from "../db.js";
 
 const mongoose = await connectDb();
 
-// Schema 
+// Schema
 const cameralocationSchema = new mongoose.Schema({
-    cameraName: String,
-    cameraUrl: String,
-    quadrant: String,   
-    cameraLocation: String,
-})
+  cameraName: String,
+  cameraUrl: String,
+  quadrant: String,
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+});
 
-// cameralocationSchema.index({ location: '2dsphere' })
+cameralocationSchema.index({ location: "2dsphere" });
 
 // Models
-const CameraLocation = mongoose.model('cameralocation', cameralocationSchema, 'cameralocations')
+const CameraLocation = mongoose.model(
+  "cameralocation",
+  cameralocationSchema,
+  "cameralocations"
+);
 
 // Functions to expose to the outside world!
-export async function createCameraLocation(cameraName, cameraUrl, quadrant, cameraLocation ) {
-    const newCameraLocation = await CameraLocation.create({
-        cameraName,
-        cameraUrl,
-        quadrant,   
-        cameraLocation,
-        
-    })
-    return newCameraLocation
+export async function createCameraLocation(
+  cameraName,
+  cameraUrl,
+  quadrant,
+  cameraLocation
+) {
+  const newCameraLocation = await CameraLocation.create({
+    cameraName,
+    cameraUrl,
+    quadrant,
+    cameraLocation,
+  });
+  return newCameraLocation;
 }
 
 export async function findAllCameraLocations() {
-    const locations = await CameraLocation.find()
-    return locations
+  const locations = await CameraLocation.find();
+  return locations;
 }
 
 export async function findCameraLocationById(id) {
-    console.log("findCameraLocationById", id)
-    const location = await CameraLocation.findById(new mongoose.Types.ObjectId(id))
-    return location
+  console.log("findCameraLocationById", id);
+  const location = await CameraLocation.findById(
+    new mongoose.Types.ObjectId(id)
+  );
+  return location;
 }
 
 export async function findCameraLocationByName(cameraNameAsset) {
-    const locations = await CameraLocation.findOne({ cameraName: cameraNameAsset })
-    return locations
+  const locations = await CameraLocation.findOne({
+    cameraName: cameraNameAsset,
+  });
+  return locations;
 }
 
 // In 1791, the metre was defined as 1 ten millionth the distance
-// between the north pole and the equator travelling through Paris. 
+// between the north pole and the equator travelling through Paris.
 // 234 years later, Tony used this formula in a sofware development
 // class focused on geographic queries.
-const METERS_PER_DEGREE = 10000000/90 
+const METERS_PER_DEGREE = 10000000 / 90;
 
 export async function findCameraLocationNear(lat, lon, distanceM) {
-    return []
+  return [];
 }
