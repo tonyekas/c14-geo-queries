3af425c6eef77956c32001f824ccdccbd6d956a2
tonyekas <tonyekas@yahoo.com>
2025-04-11 13:28:55 -0600

    made changes to the DB connection string

--- server/db.js
+++ server/db.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
-const mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/c14-geo-queries'
+// const mongo_uri = process.env.MONGO_URI || 'mongodb://localhost:27017/c14-geo-queries'
+const mongo_uri =
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/c14-geo-queries";
 
-let connectionPromise = null
+let connectionPromise = null;
 
 export async function connectDb() {
-    if (!connectionPromise) {
-        connectionPromise = mongoose.connect(mongo_uri);
-    }
-    return await connectionPromise
+  if (!connectionPromise) {
+    connectionPromise = mongoose.connect(mongo_uri);
+  }
+  return await connectionPromise;
 }
 
 export async function disconnectDb() {
-    if (connectionPromise) {
-        const mongoose = await connectionPromise
-        await mongoose.connection.close()
-        connectionPromise = null
-    }
+  if (connectionPromise) {
+    const mongoose = await connectionPromise;
+    await mongoose.connection.close();
+    connectionPromise = null;
+  }
 }
